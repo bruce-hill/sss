b542bb88bc83477bb4a5d19020da1736f1a8b350
Bruce Hill <bruce@bruce-hill.com>
2023-11-12 14:41:04 -0500

    Added parse_type_str() and parse_expression_str()

--- parse.c
+++ parse.c
@@ -2396,4 +2396,26 @@ ast_t *parse_type(sss_file_t *file, jmp_buf *on_err) {
     return ast;
 }
 
+ast_t *parse_type_str(const char *str) {
+    return parse_type(sss_spoof_file("<type>", str), NULL);
+}
+
+ast_t *parse_expression_str(const char *str) {
+    sss_file_t *file = sss_spoof_file("<expression>", str);
+    parse_ctx_t ctx = {
+        .file=file,
+        .on_err=NULL,
+    };
+
+    const char *pos = file->text;
+    whitespace(&pos);
+    ast_t *ast = parse_extended_expr(&ctx, pos);
+    pos = ast->end;
+    whitespace(&pos);
+    if (strlen(pos) > 0) {
+        parser_err(&ctx, pos, pos + strlen(pos), "I couldn't parse this part of the expression");
+    }
+    return ast;
+}
+
 // vim: ts=4 sw=0 et cino=L2,l1,(0,W4,m1,\:0
--- parse.h
+++ parse.h
@@ -8,5 +8,7 @@
 
 ast_t *parse_file(sss_file_t *file, jmp_buf *on_err);
 ast_t *parse_type(sss_file_t *file, jmp_buf *on_err);
+ast_t *parse_type_str(const char *str);
+ast_t *parse_expression_str(const char *str);
 
 // vim: ts=4 sw=0 et cino=L2,l1,(0,W4,m1,\:0
