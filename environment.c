d54ea88e5383b7b113087172ebe59878d428515a
Bruce Hill <bruce@bruce-hill.com>
2023-11-12 20:23:34 -0500

    Fix deref syntax

--- environment.c
+++ environment.c
@@ -457,14 +457,14 @@ table_t *get_namespace(env_t *env, sss_type_t *t)
         if (t->tag == TableType) {
             sss_type_t *key_t = Match(t, TableType)->key_type;
             sss_type_t *value_t = Match(t, TableType)->value_type;
-            load_method(env, ns, "remove", "Table_remove", heap_strf("func(t:&%T, key:&(read-only)%T, _type=typeof(*t))->Void", t, key_t));
-            load_method(env, ns, "pop", "Table_remove", heap_strf("func(t:&%T, _key=!%T, _type=typeof(*t))->Void", t, key_t));
-            load_method(env, ns, "set", "Table_set", heap_strf("func(t:&%T, key:&(read-only)%T, value:&(read-only)%T, _type=typeof(*t))->Void", t, key_t, value_t));
-            load_method(env, ns, "get", "Table_get", heap_strf("func(t:&(read-only)%T, key:&(read-only)%T, _type=typeof(*t))->?%T", t, key_t, value_t));
-            load_method(env, ns, "get_raw", "Table_get_raw", heap_strf("func(t:&(read-only)%T, key:&(read-only)%T, _type=typeof(*t))->?%T", t, key_t, value_t));
+            load_method(env, ns, "remove", "Table_remove", heap_strf("func(t:&%T, key:&(read-only)%T, _type=typeof(t[]))->Void", t, key_t));
+            load_method(env, ns, "pop", "Table_remove", heap_strf("func(t:&%T, _key=!%T, _type=typeof(t[]))->Void", t, key_t));
+            load_method(env, ns, "set", "Table_set", heap_strf("func(t:&%T, key:&(read-only)%T, value:&(read-only)%T, _type=typeof(t[]))->Void", t, key_t, value_t));
+            load_method(env, ns, "get", "Table_get", heap_strf("func(t:&(read-only)%T, key:&(read-only)%T, _type=typeof(t[]))->?%T", t, key_t, value_t));
+            load_method(env, ns, "get_raw", "Table_get_raw", heap_strf("func(t:&(read-only)%T, key:&(read-only)%T, _type=typeof(t[]))->?%T", t, key_t, value_t));
             load_method(env, ns, "clear", "Table_clear", heap_strf("func(t:&%T)->Void", t));
             load_method(env, ns, "mark_copy_on_write", "Table_mark_copy_on_write", heap_strf("func(t:&%T)->Void", t));
-            load_method(env, ns, "entry", "Table_entry", heap_strf("func(t:&(read-only)%T, n:Int, _type=typeof(*t))->?struct(key:%T, value:%T)", t, key_t, value_t));
+            load_method(env, ns, "entry", "Table_entry", heap_strf("func(t:&(read-only)%T, n:Int, _type=typeof(t[]))->?struct(key:%T, value:%T)", t, key_t, value_t));
         } else if (t->tag == ArrayType) {
             sss_type_t *item_t = Match(t, ArrayType)->item_type;
             load_method(env, ns, "insert", "Array_insert",
