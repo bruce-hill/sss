49f7dc523627f31339cfe2a15cd3559dff7da4c7
Bruce Hill <bruce@bruce-hill.com>
2023-11-12 20:18:42 -0500

    Add table type

--- compile/types.c
+++ compile/types.c
@@ -43,13 +43,48 @@ gcc_lvalue_t *get_type_lvalue(env_t *env, sss_type_t *t)
 
             gcc_rvalue_t *rvalues[] = {
                 gcc_str(env->ctx, key),
-                gcc_rvalue_size(env->ctx, gcc_sizeof(env, item_type)),
-                gcc_rvalue_size(env->ctx, gcc_alignof(env, item_type)),
+                gcc_rvalue_size(env->ctx, sizeof(array_t)),
+                gcc_rvalue_size(env->ctx, alignof(array_t)),
                 gcc_rvalue_int32(env->ctx, ArrayInfo),
                 get_type_pointer(env, item_type),
             };
 
             gcc_lvalue_t *global = gcc_global(env->ctx, NULL, GCC_GLOBAL_INTERNAL, gcc_struct_as_type(gcc_struct), fresh("ArrayType"));
+            gcc_global_set_initializer_rvalue(
+                global,
+                gcc_struct_constructor(env->ctx, NULL, gcc_struct_as_type(gcc_struct), sizeof(fields)/sizeof(fields[0]), fields, rvalues));
+
+            lval = gcc_rvalue_dereference(gcc_cast(env->ctx, NULL, gcc_lvalue_address(global, NULL), gcc_get_ptr_type(type_gcc_t)), NULL);
+            break;
+        }
+        case TableType: {
+            sss_type_t *key_type = Match(t, TableType)->key_type;
+            sss_type_t *value_type = Match(t, TableType)->value_type;
+            gcc_type_t *type_gcc_t = sss_type_to_gcc(env, Type(TypeType));
+            gcc_field_t *fields[] = {
+                gcc_new_field(env->ctx, NULL, gcc_type(env->ctx, STRING), "name"),
+                gcc_new_field(env->ctx, NULL, gcc_type(env->ctx, SIZE), "size"),
+                gcc_new_field(env->ctx, NULL, gcc_type(env->ctx, SIZE), "align"),
+                gcc_new_field(env->ctx, NULL, gcc_type(env->ctx, INT32), "tag"),
+                gcc_new_field(env->ctx, NULL, gcc_get_ptr_type(type_gcc_t), "key_type"),
+                gcc_new_field(env->ctx, NULL, gcc_get_ptr_type(type_gcc_t), "value_type"),
+                gcc_new_field(env->ctx, NULL, gcc_type(env->ctx, SIZE), "entry_size"),
+                gcc_new_field(env->ctx, NULL, gcc_type(env->ctx, SIZE), "value_offset"),
+            };
+            gcc_struct_t *gcc_struct = gcc_new_struct_type(env->ctx, NULL, "TableType", sizeof(fields)/sizeof(fields[0]), fields);
+
+            gcc_rvalue_t *rvalues[] = {
+                gcc_str(env->ctx, key),
+                gcc_rvalue_size(env->ctx, sizeof(table_t)),
+                gcc_rvalue_size(env->ctx, alignof(table_t)),
+                gcc_rvalue_int32(env->ctx, ArrayInfo),
+                get_type_pointer(env, key_type),
+                get_type_pointer(env, value_type),
+                gcc_rvalue_size(env->ctx, gcc_sizeof(env, table_entry_type(t))),
+                gcc_rvalue_size(env->ctx, gcc_alignof(env, table_entry_type(t))),
+            };
+
+            gcc_lvalue_t *global = gcc_global(env->ctx, NULL, GCC_GLOBAL_INTERNAL, gcc_struct_as_type(gcc_struct), fresh("TableType"));
             gcc_global_set_initializer_rvalue(
                 global,
                 gcc_struct_constructor(env->ctx, NULL, gcc_struct_as_type(gcc_struct), sizeof(fields)/sizeof(fields[0]), fields, rvalues));
